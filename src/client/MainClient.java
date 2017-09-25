99c6f5cb5017a5676be997ee0650ef10a619d1e8
Chris Kingdon <Chris Kingdon>
2017-09-25 15:16:51 -0400

    Adding client code from tutorial branch.

--- src/client/MainClient.java
+++ src/client/MainClient.java
@@ -1,9 +1,48 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class MainClient {
 
 	public static void main(String args[]) {
-		//Test code please ignore
-		System.out.println("Hello World!");
+		//If no hostname provided in command line abort
+		if(args.length != 2) {
+			System.out.println("Invalid hostname! Aborting...");
+			System.out.println("Usage: java project1.jar <host name> <port number>");
+			System.exit(1);
+		}
+		
+		String hostName = args[0];
+		int portNumber = Integer.parseInt(args[1]);
+		
+		try (
+				Socket sock = new Socket(hostName, portNumber);
+				PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));	
+		){
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			String serverMessage;
+			String clientMessage;
+			
+			while((serverMessage = in.readLine()) != null) {
+				System.out.println("Server: " + serverMessage);
+				
+				clientMessage = stdIn.readLine();
+				if (clientMessage != null) {
+					System.out.println("Client: " + clientMessage);
+					out.println(clientMessage);
+				}
+			}
+		} catch (UnknownHostException e) {
+			System.err.println("Unable to connect to host at: " + hostName + ":" + portNumber);
+			System.exit(1);
+		} catch (IOException e) {
+			System.exit(1);
+		}
 	}
 }
