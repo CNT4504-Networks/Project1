cb2e44fa726ea8d15ef18bf7f6aafdb7521b6f97
Jerrad Monagan <jerradmonagan@gmail.com>
2017-10-09 11:21:18 -0400

    makes threads but just prints out text

--- src/client/SimClient.java
+++ src/client/SimClient.java
@@ -9,16 +9,36 @@ public class SimClient {
 			System.err.println("Usage: java -jar simClient.jar <number of clients>");
 			System.exit(1);
 		}
-		
 		int numClients = Integer.parseInt(args[0]);
-		try {
-			MainClient client = new MainClient();
-			String arguments[] = {"192.168.100.107", "9000", "7", "4", "1"};
-			client.main(arguments);
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
-		
-		
+		System.out.println(Thread.currentThread().getName());
+	    for(int i=0; i<numClients; i++){
+	      new Thread("" + i){
+	        public void run(){
+	          System.out.println("Thread: " + getName() + " running");
+	          
+	        }
+	      }
+	      .start();
+	      }
+	    
+//		int numClients = Integer.parseInt(args[0]);
+//		try {
+//			MainClient client = new MainClient();
+//			Thread clientThread = new Thread();
+//			for (int i=0;i<numClients;i++){
+//			clientThread.start();
+//			System.out.println ("Thread " +Thread.currentThread().getId() +" is running");
+//			
+//			}
+//		} catch (Exception e) {
+//			System.err.println(e.getMessage());
+//		}
+//		runClients();
+//		
+	}
+	private static void runClients(){
+		MainClient client = new MainClient();
+		String arguments[] = {"192.168.100.107", "9000", "7", "4", "1"};
+		MainClient.main(arguments);	
 	}
 }//end Class SimClient
