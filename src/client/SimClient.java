61333e255b6ae9cc5ed9d73c7ee635954c490545
Jerrad Monagan <jerradmonagan@gmail.com>
2017-10-10 11:13:19 -0400

    added timer in milliseconds for report

--- src/client/SimClient.java
+++ src/client/SimClient.java
@@ -1,9 +1,12 @@
 package client;
 
+import java.io.IOException;
+
 public class SimClient {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     //Need to do netstat & date command
+	  
     if(args.length != 1) {
       System.err.println("You must enter the number of simulations to run");
       System.err.println("Usage: java -jar simClient.jar <number of clients>");
@@ -12,22 +15,24 @@ public class SimClient {
 
     int numClients = Integer.parseInt(args[0]);
     Thread newThread;
-
-    for (int i = 0; i < numClients; i++) {
     System.out.println("New Thread");
+    for (int i = 0; i < numClients; i++) {  	
       newThread = new Thread(new ClientThread());
-      newThread.start();
-    }
-  }
+      newThread.start();   
+    }    
+   }
 }//end Class SimClient
 
 class ClientThread implements Runnable {
   public void run() {
     System.out.println("New Thread");
     try {
+      long startTime =System.currentTimeMillis();
       MainClient client = new MainClient();
       String arguments[] = {"192.168.100.107", "9000", "7", "4", "1"};
       client.main(arguments);
+      long estimatedTime = System.currentTimeMillis() - startTime;
+      System.out.println(estimatedTime + " Milliseconds to complete");
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
